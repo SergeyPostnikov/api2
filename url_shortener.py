eee7e14514012597e417ebf0be430e78090112e9
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-08-19 20:47:27 +0300

    names of variables changed

--- url_shortener.py
+++ url_shortener.py
@@ -7,32 +7,32 @@ from dotenv import load_dotenv
 
 def count_clicks(token, bitlink):
     headers = {'Authorization': token}
-    host = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    response = requests.get(host, headers=headers)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json().get("total_clicks")
 
 
 def shorten_link(token, url):
     headers = {'Authorization': token}
-    host = 'https://api-ssl.bitly.com/v4/shorten'
-    response = requests.post(host, headers=headers, json={'long_url': url})
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    response = requests.post(url, headers=headers, json={'long_url': url})
     response.raise_for_status()
     return response.json().get('link') 
 
 
 def long_link(token, bitlink):
     headers = {'Authorization': token}
-    host = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
-    response = requests.get(host, headers=headers)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json().get('long_url')
 
 
 def is_bitlink(link):
     headers = {'Authorization': token}
-    host = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
-    response = requests.get(host, headers=headers)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json().get('long_url')
 
@@ -42,11 +42,11 @@ if __name__ == '__main__':
     token = os.getenv('TOKEN')  
     url = input('Введите ссылку: ')
 
-    parse_result = urlparse(url) 
-    if parse_result.hostname == 'bit.ly':
+    parsed_link = urlparse(url) 
+    if parsed_link.hostname == 'bit.ly':
         print(long_link(
             token=token, 
-            bitlink=f'{parse_result.hostname}{parse_result.path}'
+            bitlink=f'{parsed_link.hostname}{parsed_link.path}'
             ))
     else:
         print('Битлинк', shorten_link(token, url))
